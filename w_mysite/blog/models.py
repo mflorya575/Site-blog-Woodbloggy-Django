f53ec0f4ecbc635b9a31953772bab44d2d7ce4f6
Michael Florya <mih2303@mail.ru>
2024-03-24 17:34:14 +0300

    create PublishedManager in blog app & custom admin

--- w_mysite/blog/models.py
+++ w_mysite/blog/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -20,6 +27,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
