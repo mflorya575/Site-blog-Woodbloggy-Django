71a28ef91e669ad7914f6c9f54b74fdf7686b908
Michael Florya <mih2303@mail.ru>
2024-03-24 22:22:17 +0300

    add templates list, detail & controllers

--- w_mysite/blog/views.py
+++ w_mysite/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
