7b2cea8a7c153755154a745d65e3a387c35c826d
Michael Florya <mih2303@mail.ru>
2024-03-25 17:00:06 +0300

    create sitemap

--- w_mysite/w_mysite/settings.py
+++ w_mysite/w_mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
 
     # Сторонние библиотеки
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
