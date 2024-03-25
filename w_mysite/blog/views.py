eda4350a872f54221fb0dde5e6bde67bcc15268d
Michael Florya <mih2303@mail.ru>
2024-03-25 20:14:15 +0300

    full text search

--- w_mysite/blog/views.py
+++ w_mysite/blog/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.http import require_POST
 from django.db.models import Count
 
 from .models import Post, Comment
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
@@ -100,7 +100,10 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
